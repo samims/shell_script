0831f433466d76be03df09de5181e3cf9409623a
Samiul Sk <sam91v@gmail.com>
2016-04-07 22:01:35 +0530

    First Commit message

---
+++ first2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#this is a comment!
+
+echo "Hello            World"    #This is a comment too
+
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello         World
+echo "Hello" World
+echo Hello "      " World
+echo "Hello \"*\"world"
+echo `hello` world
+echo 'hello' world
+
